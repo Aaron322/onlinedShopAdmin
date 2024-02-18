b71d5a44df84584b8d49d2a7f87c4883c86914c6
Aaron <2000huhaoyu@gmail.com>
2024-02-18 15:19:34 +1100

    remove products, list products add products on admin end

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -19,6 +19,37 @@ const AddProduct = () => {
   }
   const addProduct = async () => {
     console.log(productDetaills)
+    let responseData
+    let product = productDetaills
+    let formData = new FormData()
+    formData.append('product', image)
+    await fetch('http://localhost:4000/upload', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+      body: formData,
+    }).then((resp) =>
+      resp.json().then((data) => {
+        responseData = data
+      })
+    )
+    if (responseData.success) {
+      product.image = responseData.image_url
+      console.log(product)
+      await fetch('http://localhost:4000/addproduct', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(product),
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          data.success ? alert('Product Added') : alert('failed')
+        })
+    }
   }
   return (
     <div className="add-product">
--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ListProduct.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ListProduct = () => {
-  return <div className="list-product">ListProduct</div>
+  const [allproducts, setAllProducts] = useState([])
+  const fetchProducts = async () => {
+    await fetch('http://localhost:4000/allproducts')
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data)
+      })
+  }
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const removeProduct = async (id) => {
+    await fetch('http://localhost:4000/removeproduct', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: id }),
+    })
+    await fetchProducts()
+  }
+  return (
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Product</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="listproduct-format-main listproduct-format"
+              >
+                <img
+                  src={product.image}
+                  alt=""
+                  className="listproduct-product-icon"
+                />
+                <p>{product.name}</p>
+                <p>${product.old_price}</p>
+                <p>${product.new_price}</p>
+                <p>{product.category}</p>
+                <FontAwesomeIcon
+                  className="cartitems-remove-icon"
+                  icon={faTrashAlt}
+                  onClick={() => {
+                    removeProduct(product.id)
+                  }}
+                />
+              </div>
+              <hr />
+            </>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default ListProduct
